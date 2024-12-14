fa5f855456b1d3417a3d71433ab2e839e934d850
Ilyash5643 <104620637+Ilyash5643@users.noreply.github.com>
2024-12-14 17:11:41 +0300

    Update 2 задание.py

--- homework2/2 задание.py
+++ homework2/2 задание.py
@@ -1,5 +1,5 @@
-first_list = [1,3,5,7]
-second_list = [2,4,6,8]
-deque = (first_list + second_list)
-deque.sort()
+from collections import deque
+first_list = deque([1,3,5,7])
+second_list = deque([2,4,6,8])
+deque = sorted((first_list + second_list))
 print(deque)
