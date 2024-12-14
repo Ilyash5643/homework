e38552eb7d978fce9aa40e3fb614a1512b2938d3
Ilyash5643 <104620637+Ilyash5643@users.noreply.github.com>
2024-12-14 16:56:02 +0300

    Update 2 задание.py

--- homework1/2 задание.py
+++ homework1/2 задание.py
@@ -1,5 +1,14 @@
-line = input('Введите текст')
-if line == line[::-1]:
+line = input()
+left = 0
+right = len(line) - 1
+check = True
+while left < right:
+    if line[left] != line[right]:
+        check = False
+        break
+    left += 1
+    right -= 1
+if check:
     print('Эта строка - палиндром')
 else:
     print('Эта строка не палиндром')
