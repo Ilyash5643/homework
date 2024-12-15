ecde356c70a2cd4b6cec7e846cb1722bdc36d3aa
Ilyash5643 <104620637+Ilyash5643@users.noreply.github.com>
2024-12-15 22:02:10 +0300

    Add files via upload

---
+++ homework4/2.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+
+size = 10
+hash_table = [[] for i in range(size)]
+
+def hash_function(key):
+    return len(key) % size
+keys = ["hello", "world", "hi", "Python", "hash"]
+for j in keys:
+    index = hash_function(j)
+    if not hash_table[index]:
+        hash_table[index] = deque()
+    if j not in hash_table[index]:
+        hash_table[index].append(j)
