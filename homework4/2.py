5d32fd427ef8c25a2a2e988f38313ede464d8aaa
Ilyash5643 <104620637+Ilyash5643@users.noreply.github.com>
2024-12-16 19:42:25 +0300

    Update 2.py

--- homework4/2.py
+++ homework4/2.py
@@ -4,8 +4,8 @@ from collections import deque
 size = 10
 hash_table = [[] for i in range(size)]
 
-def hash_function(key):
-    return len(key) % size
+def hash_function(j):
+    return len(j) % size
 keys = ["hello", "world", "hi", "Python", "hash"]
 for j in keys:
     index = hash_function(j)
