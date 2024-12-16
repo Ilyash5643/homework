59980dfd418015be558e59075e1bf4a23a2f6ac2
Ilyash5643 <104620637+Ilyash5643@users.noreply.github.com>
2024-12-16 21:26:28 +0300

    Update ЛР.py

--- homework7/ЛР.py
+++ homework7/ЛР.py
@@ -1,18 +1,12 @@
 class Polynomial:
     def __init__(self, coefficients):
-        """
-        Создает полином на основе списка коэффициентов.
-        coefficients: список коэффициентов [c0, c1, ..., cn], где c0 - свободный член.
-        """
         self._coefficients = coefficients[::-1]
 
     @property
     def degree(self):
-        """Возвращает степень полинома."""
         return len(self._coefficients) - 1
 
     def __repr__(self):
-        """Строковое представление полинома."""
         terms = []
         for i, coeff in enumerate(self._coefficients):
             if coeff == 0:
@@ -23,11 +17,9 @@ class Polynomial:
         return " + ".join(terms) if terms else "0"
 
     def __call__(self, x):
-        """Вычисляет значение полинома в точке x."""
         return self.evaluate(x)
 
     def __add__(self, other):
-        """Сложение двух полиномов."""
         max_len = max(len(self._coefficients), len(other._coefficients))
         result_coeffs = [0] * max_len
         for i in range(max_len):
@@ -37,7 +29,6 @@ class Polynomial:
         return Polynomial(result_coeffs[::-1])
 
     def __sub__(self, other):
-        """Вычитание двух полиномов."""
         max_len = max(len(self._coefficients), len(other._coefficients))
         result_coeffs = [0] * max_len
         for i in range(max_len):
@@ -47,7 +38,6 @@ class Polynomial:
         return Polynomial(result_coeffs[::-1])
 
     def __mul__(self, other):
-        """Умножение двух полиномов."""
         result_coeffs = [0] * (len(self._coefficients) + len(other._coefficients) - 1)
         for i, coeff1 in enumerate(self._coefficients):
             for j, coeff2 in enumerate(other._coefficients):
@@ -55,14 +45,12 @@ class Polynomial:
         return Polynomial(result_coeffs[::-1])
 
     def derivative(self):
-        """Вычисляет производную полинома."""
         if self.degree == 0:
             return Polynomial([0])
         deriv_coeffs = [self._coefficients[i] * (self.degree - i) for i in range(self.degree)]
         return Polynomial(deriv_coeffs[::-1])
 
     def evaluate(self, x):
-        """Вычисляет значение полинома в точке x."""
         result = 0
         for coeff in self._coefficients:
             result = result * x + coeff
@@ -70,19 +58,13 @@ class Polynomial:
 
 class QuadraticPolynomial(Polynomial):
     def __init__(self, coefficients):
-        """
-        Квадратичный полином: должен быть степени 2.
-        coefficients: список коэффициентов [c, b, a] для ax^2 + bx + c.
-        """
         super().__init__(coefficients)
 
     def discriminant(self):
-        """Вычисляет дискриминант (b^2 - 4ac)."""
         a, b, c = self._coefficients
         return b ** 2 - 4 * a * c
 
     def find_roots(self):
-        """Находит корни квадратичного уравнения."""
         a, b, c = self._coefficients
         disc = self.discriminant()
         root1 = (-b + disc ** 0.5) / (2 * a)
